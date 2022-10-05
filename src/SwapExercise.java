4dd613970db0dfc789b1b7015fe2d45481bdc719
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-05 09:20:20 +0900

    a와 b의 값을 swap

---
+++ src/SwapExercise.java
@@ -0,0 +1,11 @@
+public class SwapExercise {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        int temp = a;
+
+        a = b;
+        b= temp;
+        System.out.printf("a:%d b:%d", a, b);
+    }
+}
